b68f4cf89be5257098ea7ee0b417069b903c808a
Paulo Henrique Heidemann <paulo_personal@Paulos-MacBook-Pro.local>
2021-12-11 12:44:16 +0000

    added callback functionality and rendering messages using mustache

--- chat app/public/js/chat.js
+++ chat app/public/js/chat.js
@@ -1,12 +1,57 @@
 const socket = io()
 
 setTimeout(function() {
-    document.querySelector('#messageForm').addEventListener('submit', (e) => {
+    //Elements
+    const $messageForm = document.querySelector('#messageForm')
+    const $messageFormInput = document.querySelector('#text')
+    const $messageFormButton = $messageForm.querySelector('button')
+
+    const $locationButton = document.querySelector('#send-location')
+
+    $messageForm.addEventListener('submit', (e) => {
         e.preventDefault()
-        socket.emit('sendMessage', document.querySelector('#text').value)
+        $messageFormButton.setAttribute('disabled', 'disabled')
+        socket.emit('sendMessage', $messageFormInput.value, (message) => {
+            console.log('Return from server', message)
+            $messageFormButton.removeAttribute('disabled')
+            $messageFormInput.value = ''
+            $messageFormInput.focus()
+        })
+    })
+
+    $locationButton.addEventListener('click', () => {
+        if(!navigator.geolocation) {
+            return alert('Geolocation not supported!')
+        }
+
+        $locationButton.setAttribute('disabled', 'disabled')
+
+        navigator.geolocation.getCurrentPosition((position) => {
+            socket.emit('sendLocation', {
+                lat: position.coords.latitude,
+                lon: position.coords.longitude
+            }, (callBackResponse) => {
+                console.log(callBackResponse)
+                $locationButton.removeAttribute('disabled')
+            })
+        })
     })
 }, 1000)
 
 socket.on('message', (message) => {
-    console.log(message)
+    const $messagesDiv = document.querySelector('#messages')
+    const messageTemplate = document.querySelector('#message-template').innerHTML
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messagesDiv.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (location) => {
+    const $messagesDiv = document.querySelector('#messages')
+    const messageTemplate = document.querySelector('#location-message-template').innerHTML
+    const html = Mustache.render(messageTemplate, {
+        location
+    })
+    $messagesDiv.insertAdjacentHTML('beforeend', html)
 })
--- chat app/src/app.js
+++ chat app/src/app.js
@@ -2,6 +2,8 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const { SocketAddress } = require('net')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -15,10 +17,27 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
 
-    socket.emit('message', 'Welcome!')
+    socket.emit('message', 'Welcome!')//message to just the emitter
+    socket.broadcast.emit('message', 'A new user has joined!') //broadcast message to everyone but the emitter
 
-    socket.on('sendMessage', (message) => {
-        io.emit('message', message)
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profane word')
+        }
+
+        io.emit('message', message) //message to everyone
+        callback('Delivered')
+    })
+
+    socket.on('sendLocation', (location, callback) => {
+        io.emit('locationMessage', `https://google.com/maps?q=${location.lat},${location.lon}`) //broadcast message to everyone but the emitter
+        callback('Location shared')
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
     })
 
     // socket.emit('countUpdated', count) // Socket emits the event to a single client, io sends to all connected clients
